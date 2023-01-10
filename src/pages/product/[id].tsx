b48e148708f79934534c09c74d49d63141c0c52e
Murillo Mesquita <mu.pracucio@gmail.com>
2023-01-10 11:15:08 -0300

    estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
   
   return (
-    <div>{JSON.stringify(query)}</div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptatem cum. Quaerat nulla iusto labore a, tempora unde dolorum. Saepe, ex totam vitae cum asperiores esse impedit nobis quod. Iste.</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
